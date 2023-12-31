97c7e18ce94793a9670ffe59d6610d8a6cc1748b
junmo95 <junmop950@gmail.com>
2024-01-01 03:13:23 +0900

    Feat : 게시판(Board) 예외 구현 - Board 기능에 대한 예외관련 객체, 핸들러 구현

--- backend/src/main/java/com/junmo/junmowebsite/global/exception/GlobalExceptionRestControllerAdvice.java
+++ backend/src/main/java/com/junmo/junmowebsite/global/exception/GlobalExceptionRestControllerAdvice.java
@@ -1,9 +1,12 @@
 package com.junmo.junmowebsite.global.exception;
 
 import com.junmo.junmowebsite.global.util.ResponseDto;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionRestControllerAdvice {
 
+    // 전체 에러 처리
     @ExceptionHandler
     public ResponseEntity<ResponseDto<Object>> serverException(RuntimeException e) {
         log.error(e.getMessage(), e);
@@ -18,6 +22,24 @@ public class GlobalExceptionRestControllerAdvice {
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ResponseDto.response(e.getMessage() + " || 서버에러발생"));
 
+    }
+
+    // Valid 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> boardValidException(
+        MethodArgumentNotValidException e
+    ) {
+        List<String> errors = new ArrayList<>();
+
+        e.getBindingResult().getFieldErrors().forEach((error) -> {
+            String errorMessage = error.getField() + " : " + error.getDefaultMessage();
+            errors.add(errorMessage);
+            log.error(errorMessage);
+        });
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ResponseDto.response("잘못된 요청입니다.", errors));
     }
 
 }
