69125bf115d04cb721873bf3be27f7009435df42
junmo95 <junmop950@gmail.com>
2024-01-01 03:11:02 +0900

    Feat : 게시판(Board) Controller 구현

--- backend/src/main/java/com/junmo/junmowebsite/domain/board/controller/BoardController.java
+++ backend/src/main/java/com/junmo/junmowebsite/domain/board/controller/BoardController.java
@@ -1,17 +1,14 @@
 package com.junmo.junmowebsite.domain.board.controller;
 
 import com.junmo.junmowebsite.domain.board.dto.response.BoardResponseDto;
-import com.junmo.junmowebsite.domain.board.dto.request.BoardCreateRequestDto;
+import com.junmo.junmowebsite.domain.board.dto.request.BoardRequestDto;
 import com.junmo.junmowebsite.domain.board.service.BoardService;
 import com.junmo.junmowebsite.global.util.ResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/boards")
@@ -20,10 +17,9 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    // C
     @PostMapping
     public ResponseEntity<ResponseDto<BoardResponseDto>> boardAdd(
-            @RequestBody @Valid BoardCreateRequestDto requestDto
+            @RequestBody @Valid BoardRequestDto requestDto
     ) {
 
         BoardResponseDto responseDto = boardService.addBoard(requestDto);
@@ -32,4 +28,56 @@ public class BoardController {
                 .status(HttpStatus.CREATED)
                 .body(ResponseDto.response("게시판 생성 완료", responseDto));
     }
+
+    @GetMapping("/{boardId}")
+    public ResponseEntity<ResponseDto<BoardResponseDto>> boardDetails(
+            @PathVariable Long boardId
+    ) {
+        BoardResponseDto response = boardService.findBoard(boardId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseDto.response("게시판 조회 완료", response));
+    }
+
+    @PatchMapping("/{boardId}")
+    public ResponseEntity<ResponseDto<BoardResponseDto>> boardModify(
+        @PathVariable Long boardId,
+        @RequestBody @Valid BoardRequestDto requestDto
+    ) {
+        BoardResponseDto responseDto = boardService.modifyBoard(boardId, requestDto);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ResponseDto.response("게시판 수정 완료", responseDto));
+    }
+
+    @DeleteMapping("/{boardId}")
+    public ResponseEntity<ResponseDto<Object>> boardRemove(
+        @PathVariable Long boardId
+    ) {
+
+        boardService.removeBoard(boardId);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ResponseDto.response("게시판 삭제 완료"));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
